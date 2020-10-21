3b109118d9a2286fdfab3b606a938582d6b43854
Hennadii Dobrovolskyi <gen.dobr@gmail.com>
2020-10-21 14:20:30 +0300

    add excel reader

--- scripts/lib/datasetfactory.py
+++ scripts/lib/datasetfactory.py
@@ -1,11 +1,13 @@
 # import PyPDF2
 from os import listdir
+import pandas as pd
 from os.path import isfile
 from os.path import join
 import datetime
 import random
 import time
 import jsonlines
+import json
 import re
 
 
@@ -85,23 +87,32 @@ def factory_time_asc(txt_file_dir, dataset_file_dir, increment_size=1, metadata=
 def __sort_citation_per_year_desc(txt_file_dir, metadata=False):
     t0 = time.time()
     # read metadata file
-    with jsonlines.open(metadata) as reader:
-        items = {str(row['id']): row for row in reader}
+    if metadata[-6:] == '.jsonl':
+        with jsonlines.open(metadata) as reader:
+            items = {str(row['id']): row for row in reader}
 
-    now_year = datetime.datetime.utcnow().year
+        now_year = datetime.datetime.utcnow().year
 
-    # get pairs (citation_index, file_path)
-    pairs = []
-    for item_id in items:
-        item = items[item_id]
-        txt_file_path = join(txt_file_dir, re.sub(r'\.pdf$', '.txt', item["pdf_file_name"]))
-        if isfile(txt_file_path):
-            citations_per_year = 0
-            try:
-                order_by = int(item["google_scholar"]["cites"]) / (now_year - int(item["year"]) + 1)
-            except:
-                order_by = 0
-            pairs.append((order_by, txt_file_path,))
+        # get pairs (citation_index, file_path)
+        pairs = []
+        for item_id in items:
+            item = items[item_id]
+            txt_file_path = join(txt_file_dir, re.sub(r'\.pdf$', '.txt', item["pdf_file_name"]))
+            if isfile(txt_file_path):
+                citations_per_year = 0
+                try:
+                    order_by = int(item["google_scholar"]["cites"]) / (now_year - int(item["year"]) + 1)
+                except:
+                    order_by = 0
+                pairs.append((order_by, txt_file_path,))
+    elif metadata[-5:] == '.xlsx':
+        items = json.loads(pd.read_excel(metadata).to_json(orient="records"))
+        pairs = []
+        for item in items:
+            order_by = item["Citations per year (GS)"]
+            txt_file_path = join(txt_file_dir, re.sub(r'\.pdf$', '.txt', item["Document file name"]))
+            if isfile(txt_file_path):
+                pairs.append((order_by, txt_file_path,))
 
     sorted_txt_files = sorted(pairs, reverse=True)
     # print(sorted_txt_files)
@@ -251,6 +262,8 @@ def __sort_time_asc(txt_file_dir, metadata=False):
     sorted_txt_files = sorted(pairs)
     # print(sorted_txt_files)
     return sorted_txt_files
+
+
 
 
 def __create_datasets(sorted_txt_files, increment_size, dataset_file_dir):
